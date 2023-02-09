3f0eb0671e5238c339435319e51fe29445207240
quyentrankhanhha <quyentrankhanhha@gmail.com>
2023-02-09 13:11:29 +0200

    ui for login and register pages

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,3 +1,46 @@
+import { Link } from 'react-router-dom'
+
 export default function Login() {
-  return <div>Login</div>
+  return (
+    <div>
+      <div className='mx-auto max-w-7xl px-4'>
+        <div className='grid grid-cols-1 py-12 lg:grid-cols-5 lg:py-32 lg:pr-10'>
+          <div className='lg:col-span2 lg:col-start-4'>
+            <form className='rounded bg-white p-10 shadow-sm'>
+              <div className='text-2xl'>Login</div>
+              <div className='mt-8'>
+                <input
+                  type='email'
+                  name='email'
+                  className='rounded-sm border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
+                  placeholder='Email'
+                />
+                <div className='min-h-[1rem]text-sm mt-1 text-red-600'></div>
+              </div>
+              <div className='mt-3'>
+                <input
+                  type='password'
+                  name='password'
+                  className='rounded-sm border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
+                  placeholder='Password'
+                />
+                <div className='min-h-[1rem]text-sm mt-1 text-red-600'></div>
+              </div>
+              <div className='mt-3'>
+                <button className='w-full bg-red-500 py-4 px-2 text-center text-sm uppercase text-white hover:bg-red-600'>
+                  Login
+                </button>
+              </div>
+              <div className='mt-8 flex items-center justify-center'>
+                <span className='text-slate-400'>Do not have an account? </span>
+                <Link to='/register' className='ml-1 text-red-400'>
+                  Register here
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
