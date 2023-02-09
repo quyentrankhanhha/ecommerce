facbfccaca741c8fdc49655d66c13e1c3faa3588
quyentrankhanhha <quyentrankhanhha@gmail.com>
2023-02-09 12:43:00 +0200

    ui for register layout

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <footer className='bg-neutral-100 py-16'></footer>
+}
---
+++ src/components/RegisterHeader/RegisterHeader.tsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom'
+
+export default function RegisterHeader() {
+  return (
+    <header className='py-5'>
+      <div className='max-auto max-w-7xl px-4'>
+        <nav className='flex items-end'>
+          <Link to='/'>
+            <img
+              src='https://tonda.qodeinteractive.com/wp-content/uploads/2018/03/logo-header-1.png'
+              alt='logo'
+              className='h-8 lg:h-11'
+            />
+          </Link>
+          <div className='ml-5 text-xl lg:text-2xl'>Register</div>
+        </nav>
+      </div>
+    </header>
+  )
+}
