d8cc0bb6a8c10fbcda907976d64edda41eb9ffec
quyentrankhanhha <quyentrankhanhha@gmail.com>
2023-02-10 13:15:47 +0200

    customer class container

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -23,7 +23,7 @@ export default function Register() {
 
   return (
     <div>
-      <div className='mx-auto max-w-7xl px-4'>
+      <div className='container'>
         <div className='grid grid-cols-1 py-12 lg:grid-cols-5 lg:py-32 lg:pr-10'>
           <div className='lg:col-span2 lg:col-start-4'>
             <form className='rounded bg-white p-10 shadow-sm' onSubmit={onSubmit} noValidate>
