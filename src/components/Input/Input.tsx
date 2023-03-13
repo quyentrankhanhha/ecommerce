434be832ae2a928ab6072d3c361d65e9326fab70
quyentrankhanhha <quyentrankhanhha@gmail.com>
2023-03-13 03:17:41 +0200

    clean input component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -11,27 +11,19 @@ interface Props extends InputHTMLAttributes<HTMLInputElement> {
 
 export default function Input({
   className,
-  type,
-  placeholder,
-  autoComplete,
   register,
   name,
   rules,
   errorMessage,
   classNameInput = 'rounded-sm border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm',
-  classNameError = 'min-h-[1.25rem]text-sm mt-1 text-red-600'
+  classNameError = 'min-h-[1.25rem]text-sm mt-1 text-red-600',
+  ...rest
 }: Props) {
   const registerResult = register && name ? register(name, rules) : {}
 
   return (
     <div className={className}>
-      <input
-        type={type}
-        className={classNameInput}
-        placeholder={placeholder}
-        autoComplete={autoComplete}
-        {...registerResult}
-      />
+      <input className={classNameInput} {...registerResult} {...rest} />
       <div className={classNameError}>{errorMessage}</div>
     </div>
   )
