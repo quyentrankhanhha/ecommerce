903965dfb3ce55b59e12a6410b0a660a93b9b50b
Ha <quyentrankhanhha@gmail.com>
2024-01-03 18:50:59 +0200

    update InputNumber and QuantityController components

--- src/components/InputNumber/InputNumber.tsx
+++ src/components/InputNumber/InputNumber.tsx
@@ -1,4 +1,4 @@
-import { InputHTMLAttributes, forwardRef } from 'react'
+import { InputHTMLAttributes, forwardRef, useState } from 'react'
 
 export interface InputNumberProps extends InputHTMLAttributes<HTMLInputElement> {
   errorMessage?: string
@@ -13,19 +13,23 @@ const InputNumber = forwardRef<HTMLInputElement, InputNumberProps>(function Inpu
     classNameInput = 'rounded-sm border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm',
     classNameError = 'min-h-[1.25rem]text-sm mt-1 text-red-600',
     onChange,
+    value = '',
     ...rest
   },
   ref
 ) {
+  const [localValue, setLocalValue] = useState<string>(value as string)
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target
-    if (/^\d+$/.test(value) || (value === '' && onChange)) {
+    if (/^\d+$/.test(value) || value === '') {
       onChange && onChange(event)
+
+      setLocalValue(value)
     }
   }
   return (
     <div className={className}>
-      <input className={classNameInput} onChange={handleChange} {...rest} ref={ref} />
+      <input className={classNameInput} onChange={handleChange} value={value || localValue} {...rest} ref={ref} />
       <div className={classNameError}>{errorMessage}</div>
     </div>
   )
--- src/components/QuantityController/QuantityController.tsx
+++ src/components/QuantityController/QuantityController.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import InputNumber, { InputNumberProps } from '../InputNumber'
 
 interface Props extends InputNumberProps {
@@ -17,6 +18,7 @@ export default function QuantityController({
   value,
   ...rest
 }: Props) {
+  const [localValue, setLocalValue] = useState<number>(Number(value || 0))
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     let _value = Number(event.target.value)
     if (max !== undefined && _value > max) {
@@ -26,22 +28,25 @@ export default function QuantityController({
     }
 
     onType && onType(_value)
+    setLocalValue(_value)
   }
 
   const increaseQuantity = () => {
-    let _value = Number(value) + 1
+    let _value = Number(value || localValue) + 1
     if (max !== undefined && _value > max) {
       _value = max
     }
     onIncrease && onIncrease(_value)
+    setLocalValue(_value)
   }
 
   const decreaseQuantity = () => {
-    let _value = Number(value) - 1
+    let _value = Number(value || localValue) - 1
     if (_value < 1) {
       _value = 1
     }
     onDecrease && onDecrease(_value)
+    setLocalValue(_value)
   }
 
   return (
@@ -62,7 +67,7 @@ export default function QuantityController({
         </svg>
       </button>
       <InputNumber
-        value={value}
+        value={value || localValue}
         className=''
         classNameError='hidden'
         classNameInput='h-8 w-14 border-t border-b border-gray-300 text-center p-1 outline-none'
