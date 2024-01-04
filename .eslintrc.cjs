8106eed649b74275ea16ea4af618567908a27c0f
Ha <quyentrankhanhha@gmail.com>
2024-01-04 16:26:26 +0200

    fix eslint error 2

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,33 +1,31 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const path = require('path')
-
 module.exports = {
   env: {
     node: true
   },
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:import/recommended',
-    'plugin:import/typescript',
-    'plugin:jsx-a11y/recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'eslint-config-prettier',
     'prettier'
   ],
-  plugins: ['react', 'prettier'],
-  settings: {
-    react: {
-      version: 'detect'
-    },
-    'import/resolver': {
-      node: {
-        paths: [path.resolve(__dirname, '')],
-        extensions: ['.js', '.jsx', '.ts', '.tsx']
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script'
       }
     }
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react/jsx-no-target-blank': 'warn',
@@ -45,5 +43,10 @@ module.exports = {
         jsxSingleQuote: true
       }
     ]
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
   }
 }
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,7 +2,6 @@ import axios, { AxiosError } from 'axios'
 import HttpStatusCode from 'src/constants/httpStatusCode.enum'
 
 export function isAxiosError<T>(error: unknown): error is AxiosError<T> {
-  // eslint-disable-next-line import/no-named-as-default-member
   return axios.isAxiosError(error)
 }
 
